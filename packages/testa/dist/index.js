13df1456071a2588215a3c3a401dec2020744d77
Your Name <you@example.com>
2024-11-23 13:39:15 +0800

    test

--- packages/testa/dist/index.js
+++
@@ -1,1 +0,0 @@
-var o=123;console.log("\u{1F680} ~ file: index.ts:2 ~ a:1",123);export{o as a};
--- packages/testa/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {a} from '@/a';
-
-console.log(`🚀 ~ file: index.ts:2 ~ a:1`, a);
-
-
-export {a};
